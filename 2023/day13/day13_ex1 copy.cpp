6cf1b12e5c7091b03f35ec143115da5db193d20c
Caroline MARION <cmarion@made-f0Br8s14.clusters.42paris.fr>
2023-12-13 20:48:14 +0100

    still not working

---
+++ 2023/day13/day13_ex1 copy.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void full_ref(int* ref, int f, int s) {
+    ref[0] = f;
+    ref[1] = s;
+}
+
+int note(vector<string> pattern) {
+    int sym_pos;
+    for (int i = pattern.size() / 2; i < pattern.size() - 1; i ++) {
+        if (pattern[i] == pattern[i + 1]) {
+            sym_pos = i;
+            break;
+        }
+    }
+}
+
+
+int main () {
+    ifstream myfile("day13_input.txt");
+    // ifstream myfile("day13_testInput.txt");
+    string          input;
+    vector<string>  h_pattern, v_pattern;
+    int             sym_sum = 0, l = 0;
+
+    if (myfile.is_open()) {
+        while (getline(myfile, input)) {
+            if (input.size() > 0) {
+                h_pattern.push_back(input);
+                if (l == 0) {
+                    for (int i = 0; i < input.length(); i ++)
+                        v_pattern.push_back(string(1, input[i]));
+                } else {
+                    for (int i = 0; i < input.length(); i ++)
+                        v_pattern[i] += input[i];
+                }
+                l ++;
+            } else {
+                int h_ref[2], v_ref[2];
+                for (int i = 0; i < h_pattern.size() - 1; i ++) {
+                    if (h_pattern[i] == h_pattern[i + 1]) {
+                        full_ref(h_ref, i, i + 1);
+                        break;
+                    }
+                }
+                for (int i = 0; i < v_pattern.size(); i ++) {
+                        if (v_pattern[i] == v_pattern[i + 1]) {
+                            full_ref(v_ref, i, i + 1);
+                            break;
+                        }
+                }
+                int f = 0;
+                if (h_ref[0] >= h_pattern.size() / 2) {
+                    for (auto p : h_pattern)
+                        cerr << p << endl;
+                    for (int i = h_ref[0] - 1, j = h_ref[1] + 1; j < h_pattern.size(); i--, j++) {
+                        if (h_pattern[i] != h_pattern[j])
+                            break;    
+                        f ++;
+                    }
+                    if (f == h_pattern.size() - h_ref[1] - 1) {
+                        cerr << "h " << h_ref[1] << endl;
+                        sym_sum += h_ref[1] * 100;
+                    }
+                }
+                f = 0;
+                if (v_ref[0] >= v_pattern.size() / 2) {
+                    for (auto p : v_pattern)
+                        cerr << p << endl;
+                    for (int i = v_ref[0] - 1, j = v_ref[1] + 1; j < v_pattern.size(); i--, j++) {
+                        if (v_pattern[i] != v_pattern[j])
+                            break;
+                        f ++;
+                    }
+                    if (f == v_pattern.size() - v_ref[1] - 1) {
+                        cerr << "v " << v_ref[1] << endl;
+                        sym_sum += v_ref[1];
+                    }
+                }
+                h_pattern.clear();
+                v_pattern.clear();
+                l = 0;
+            }
+        }
+        int h_ref[2], v_ref[2];
+        for (int i = 0; i < h_pattern.size() - 1; i ++) {
+            if (h_pattern[i] == h_pattern[i + 1]) {
+                full_ref(h_ref, i, i + 1);
+                break;
+            }
+        }
+        for (int i = 0; i < v_pattern.size(); i ++) {
+                if (v_pattern[i] == v_pattern[i + 1]) {
+                    full_ref(v_ref, i, i + 1);
+                    break;
+                }
+        }
+        int f = 0;
+        if (h_ref[0] >= h_pattern.size() / 2) {
+            for (int i = h_ref[0] - 1, j = h_ref[1] + 1; i >= 0 && j < h_pattern.size(); i--, j++) {
+                if (h_pattern[i] != h_pattern[j])
+                    break;
+                f ++;
+            }
+            if (f == h_pattern.size() - h_ref[1] - 1)
+                sym_sum += h_ref[1] * 100;
+        }
+        f = 0;
+        if (v_ref[0] >= v_pattern.size() / 2) {
+            for (int i = v_ref[0] - 1, j = v_ref[1] + 1; i >= 0 && j < v_pattern.size(); i--, j++) {
+                if (v_pattern[i] != v_pattern[j])
+                    break;
+                f ++;
+            }
+            if (f == v_pattern.size() - v_ref[1] - 1)
+                sym_sum += v_ref[1];
+        }
+    }
+}
--- 2023/day13/day13_ex1.cpp
+++ 2023/day13/day13_ex1.cpp
@@ -10,6 +10,31 @@ void full_ref(int* ref, int f, int s) {
     ref[1] = s;
 }
 
+int note(vector<string> pattern) {
+    for (auto p : pattern)
+        cerr << p << endl;
+    cerr << pattern.size() / 2 << endl;
+    for (int s = 0; s < pattern.size() - 1; s ++) {
+        if (pattern[s] == pattern[s + 1]) {
+            int i = s - 1;
+            int j = s + 2;
+            cerr << " ------ " << endl;
+            while(j < pattern.size()) {
+                cerr << pattern[i] << " " << pattern[j] << endl;
+                if (pattern[i] != pattern[j])
+                    break;
+                i--;
+                j++;
+            }
+            if (j == pattern.size()) {
+                cerr << "test" << endl;
+                return s + 1;
+            }
+        }
+    }
+    return 0;
+}
+
 
 int main () {
     ifstream myfile("day13_input.txt");
@@ -31,84 +56,33 @@ int main () {
                 }
                 l ++;
             } else {
-                int h_ref[2], v_ref[2];
-                for (int i = 0; i < h_pattern.size() - 1; i ++) {
-                    if (h_pattern[i] == h_pattern[i + 1]) {
-                        full_ref(h_ref, i, i + 1);
-                        break;
-                    }
+                int h = note(h_pattern);
+                int v = note(v_pattern);
+                if (h) {
+                    cerr << "h " << h << endl;
+                    sym_sum += h * 100;
                 }
-                for (int i = 0; i < v_pattern.size(); i ++) {
-                        if (v_pattern[i] == v_pattern[i + 1]) {
-                            full_ref(v_ref, i, i + 1);
-                            break;
-                        }
+                if (v) {
+                    cerr << "v " << v << endl;
+                    sym_sum += v;
                 }
-                int f = 0;
-                if (h_ref[0] >= h_pattern.size() / 2) {
-                    for (auto p : h_pattern)
-                        cerr << p << endl;
-                    for (int i = h_ref[0] - 1, j = h_ref[1] + 1; j < h_pattern.size(); i--, j++) {
-                        if (h_pattern[i] != h_pattern[j])
-                            break;    
-                        f ++;
-                    }
-                    if (f == h_pattern.size() - h_ref[1] - 1) {
-                        cerr << "h " << h_ref[1] << endl;
-                        sym_sum += h_ref[1] * 100;
-                    }
-                }
-                f = 0;
-                if (v_ref[0] >= v_pattern.size() / 2) {
-                    for (auto p : v_pattern)
-                        cerr << p << endl;
-                    for (int i = v_ref[0] - 1, j = v_ref[1] + 1; j < v_pattern.size(); i--, j++) {
-                        if (v_pattern[i] != v_pattern[j])
-                            break;
-                        f ++;
-                    }
-                    if (f == v_pattern.size() - v_ref[1] - 1) {
-                        cerr << "v " << v_ref[1] << endl;
-                        sym_sum += v_ref[1];
-                    }
-                }
+                cerr << endl;
                 h_pattern.clear();
                 v_pattern.clear();
                 l = 0;
             }
         }
-        int h_ref[2], v_ref[2];
-        for (int i = 0; i < h_pattern.size() - 1; i ++) {
-            if (h_pattern[i] == h_pattern[i + 1]) {
-                full_ref(h_ref, i, i + 1);
-                break;
-            }
-        }
-        for (int i = 0; i < v_pattern.size(); i ++) {
-                if (v_pattern[i] == v_pattern[i + 1]) {
-                    full_ref(v_ref, i, i + 1);
-                    break;
-                }
-        }
-        int f = 0;
-        if (h_ref[0] >= h_pattern.size() / 2) {
-            for (int i = h_ref[0] - 1, j = h_ref[1] + 1; i >= 0 && j < h_pattern.size(); i--, j++) {
-                if (h_pattern[i] != h_pattern[j])
-                    break;
-                f ++;
-            }
-            if (f == h_pattern.size() - h_ref[1] - 1)
-                sym_sum += h_ref[1] * 100;
-        }
-        f = 0;
-        if (v_ref[0] >= v_pattern.size() / 2) {
-            for (int i = v_ref[0] - 1, j = v_ref[1] + 1; i >= 0 && j < v_pattern.size(); i--, j++) {
-                if (v_pattern[i] != v_pattern[j])
-                    break;
-                f ++;
-            }
-            if (f == v_pattern.size() - v_ref[1] - 1)
-                sym_sum += v_ref[1];
-        }
     }
+    int h = note(h_pattern);
+    int v = note(v_pattern);
+    if (h) {
+        cerr << "h " << h << endl;
+        sym_sum += h * 100;
+    }
+    if (v){
+        cerr << "v " << v << endl;
+        sym_sum += v;
+    }
+    cerr << endl;
+    cout << sym_sum << endl;
 }
