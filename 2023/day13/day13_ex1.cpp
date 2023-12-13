f61b9bf451231778ee642d028f65952a88966929
Caroline <cmarion.mq@gmail.com>
2023-12-13 22:57:17 +0100

    day13 ex1 ok

--- 2023/day13/day13_ex1.cpp
+++ 2023/day13/day13_ex1.cpp
@@ -5,36 +5,21 @@
 
 using namespace std;
 
-void full_ref(int* ref, int f, int s) {
-    ref[0] = f;
-    ref[1] = s;
-}
-
 int note(vector<string> pattern) {
-    for (auto p : pattern)
-        cerr << p << endl;
-    cerr << pattern.size() / 2 << endl;
     for (int s = 0; s < pattern.size() - 1; s ++) {
         if (pattern[s] == pattern[s + 1]) {
             int i = s - 1;
             int j = s + 2;
-            cerr << " ------ " << endl;
-            while(j < pattern.size()) {
-                cerr << pattern[i] << " " << pattern[j] << endl;
-                if (pattern[i] != pattern[j])
-                    break;
+            while(i >= 0 && j < pattern.size() && pattern[i] == pattern[j]) {
                 i--;
                 j++;
             }
-            if (j == pattern.size()) {
-                cerr << "test" << endl;
+            if (i == -1 || j == pattern.size())
                 return s + 1;
-            }
         }
     }
     return 0;
 }
-
 
 int main () {
     ifstream myfile("day13_input.txt");
@@ -56,33 +41,13 @@ int main () {
                 }
                 l ++;
             } else {
-                int h = note(h_pattern);
-                int v = note(v_pattern);
-                if (h) {
-                    cerr << "h " << h << endl;
-                    sym_sum += h * 100;
-                }
-                if (v) {
-                    cerr << "v " << v << endl;
-                    sym_sum += v;
-                }
-                cerr << endl;
+                sym_sum += note(h_pattern) * 100 + note(v_pattern);
                 h_pattern.clear();
                 v_pattern.clear();
                 l = 0;
             }
         }
     }
-    int h = note(h_pattern);
-    int v = note(v_pattern);
-    if (h) {
-        cerr << "h " << h << endl;
-        sym_sum += h * 100;
-    }
-    if (v){
-        cerr << "v " << v << endl;
-        sym_sum += v;
-    }
-    cerr << endl;
+    sym_sum += note(h_pattern) * 100 + note(v_pattern);;
     cout << sym_sum << endl;
 }
